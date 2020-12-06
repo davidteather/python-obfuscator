e898635dae4a2e0a11a7bf854703afff76d14a68
davidteather <davidteather@users.noreply.github.com>
2020-12-06 01:49:49 -0600

    Update test

--- tests/test_module.py
+++ tests/test_module.py
@@ -5,10 +5,11 @@ def test_module_methods():
     obfuscate = python_obfuscator.obfuscator()
 
     code = """
-    def add(x, y):
-    \treturn x+y
-    assert add(10,10) == 20
-    assert add(0,0) == 0
+    v1 = 0
+    v2 = 0
+    v4 = 10
+    assert v4 + v4 == 20
+    assert v1 + v2 == 0
     """.replace(
         "    ", ""
     )
